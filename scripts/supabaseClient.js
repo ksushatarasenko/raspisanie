6fda145e0dc02a5ff22138922004c733eea2f431
Oksana <ksushatarasenko@gmail.com>
2025-12-07 07:19:53 +0100

    new

--- scripts/supabaseClient.js
+++ scripts/supabaseClient.js
@@ -1,7 +1,8 @@
 
 /// scripts/supabaseClient.js
 // единый клиент Supabase — положи сюда свой anon key (не выкладывай публично)
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
+import { createClient } from 'https://cdn.skypack.dev/@supabase/supabase-js';
+
 
 const supabaseUrl = 'https://xhvuxrifdquyeuffoqju.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhodnV4cmlmZHF1eWV1ZmZvcWp1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQwOTY3NDksImV4cCI6MjA3OTY3Mjc0OX0.CFmO_mLGp4cX89lJcs-1CO0RlEXpsTQhy_yVp8XwqXQ';
